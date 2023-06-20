870a5d251f38721abf2f9f9192b537dcbfcea7c8
Wilson Dev <wilson.hch@hynkor.com>
2023-06-19 20:47:51 -0500

    Consumiendo la FakeStore API para pintar cards

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,14 +1,14 @@
-const Card = ()=>{
+const Card = (data)=>{
     return(
         <div className="bg-white cursor-pointer w-56 h-60 rounded-lg ">
             <figure className="relative mb-2 w-full h-4/5">
-                <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-sx m-2 px-3 py-0.5">Electronics</span>
-                <img className="w-full h-full object-cover rounded-lg" src="https://images.unsplash.com/photo-1560617544-b4f287789e24?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2VsdWxhcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60" alt="Cellphone" />
+                <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-sx m-2 px-3 py-0.5">{data.data.category.name}</span>
+                <img className="w-full h-full object-cover rounded-lg" src={data.data.images} alt={data.data.title} />
                 <div className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1">+</div>
             </figure>
             <p className="flex justify-between">
-                <span className="text-sm font-light">Cellphone 📱</span>
-                <span className="text-lg font-medium">$ 300.00</span>
+                <span className="text-sm font-light">{data.data.title}</span>
+                <span className="text-lg font-medium">$ {data.data.price}</span>
             </p>
         </div>
     )
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,9 +1,23 @@
+import { useState,useEffect } from "react"
 import { Card } from "../../Components/Card"
+
 function Home() {
+  const [items,setItems]=useState(null)
+  useEffect(()=>{
+    fetch("https://api.escuelajs.co/api/v1/products") // fetch solicita los datos (productos a la API)
+      .then(response=>response.json())
+      .then(data=>setItems(data))
+  },[])
     return (
       <>
           Home
-          <Card/>        
+          <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+            {
+              items?.map((item)=>(
+                <Card key={item.id} data={item}/>
+              ))
+            } 
+          </div>       
       </>
     )
   }
