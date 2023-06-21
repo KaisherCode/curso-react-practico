00b595c72efce73df46984f75d67ca675443e8aa
Wilson Dev <wilson.hch@hynkor.com>
2023-06-21 16:56:31 -0500

    Página de MyOrders: Lista de órdenes

--- src/Pages/MyOrders/index.jsx
+++ src/Pages/MyOrders/index.jsx
@@ -1,9 +1,29 @@
-
+import { useContext } from 'react'
+import { Link } from 'react-router-dom'
+import {Layout} from '../../Components/Layout'
+import {ShopingCartContext} from '../../Context'
+import { OrdersCard } from '../../Components/OrdersCart'
 function MyOrders() {
+  const context = useContext(ShopingCartContext)
+
 
     return (
       <>
-        MyOrders
+      <Layout>
+        <div className='flex w-80 justify-center items-center relative'>
+          <h1>My Orders</h1>
+        </div>
+        {
+          context.order.map((order,index)=>{
+            <Link key={index} to={`/my-orders/${order.id}`}>
+                <OrdersCard 
+                totalPrice={order.totalPrice}
+                totalProducts={order.totalProducts}
+                />    
+            </Link>
+          })
+        }
+      </Layout>
       </>
     )
   }
