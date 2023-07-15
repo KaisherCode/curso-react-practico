436846f5e29e1974ef905365cf9b584253e13133
Wilson Dev <wilson.hch@hynkor.com>
2023-07-15 18:06:17 -0500

    Inplementación del componente footer

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,17 +1,18 @@
 import { useContext } from 'react'
-import { useRoutes,BrowserRouter,Navigate } from 'react-router-dom'
-import { ShoppinCartProvider,initializeLocalStorage,ShoppinCartContext } from '../../Context'
+import { useRoutes, BrowserRouter, Navigate } from 'react-router-dom'
+import { ShoppinCartProvider, initializeLocalStorage, ShoppinCartContext } from '../../Context'
 import Home from '../Home'
 import MyAcount from '../MyAcount'
 import MyOrder from '../MyOrder'
 import MyOrders from '../MyOrders'
 import NotFound from '../NotFound'
 import SignIn from '../SignIn'
-import {Navbar} from '../../Components/Navbar'
+import { Navbar } from '../../Components/Navbar'
+import { Footer } from '../../Components/Footer'
 import { CheckoutSideMenu } from '../../Components/CheckoutSideMenu'
 import './App.css'
 
-const AppRouters = ()=>{
+const AppRouters = () => {
   const context = useContext(ShoppinCartContext)
   // Account
   const account = localStorage.getItem('account')
@@ -20,24 +21,24 @@ const AppRouters = ()=>{
   const signOut = localStorage.getItem('sign-out')
   const parsedSignOut = JSON.parse(signOut)
   // Has an account
-  const noAccountInLocalStorage = parsedAccount? Object.keys(parsedAccount).length===0:true
-  const noAccountInLocalState = Object.keys(context.account).length===0
+  const noAccountInLocalStorage = parsedAccount ? Object.keys(parsedAccount).length === 0 : true
+  const noAccountInLocalState = Object.keys(context.account).length === 0
   const hasUserAnAccount = !noAccountInLocalStorage || !noAccountInLocalState
   const isUserSignOut = context.signOut || parsedSignOut
 
   let routes = useRoutes([
-    {path:'/', element:<Home/>},
-    {path:'/clothes', element:hasUserAnAccount && !isUserSignOut?<Home/>:<Navigate replace to={'/sign-in'}/>},
-    {path:'/electronics', element:hasUserAnAccount && !isUserSignOut?<Home/>:<Navigate replace to={'/sign-in'}/>},
-    {path:'/furnitures', element:hasUserAnAccount && !isUserSignOut?<Home/>:<Navigate replace to={'/sign-in'}/>},
-    {path:'/toys', element:hasUserAnAccount && !isUserSignOut?<Home/>:<Navigate replace to={'/sign-in'}/>},
-    {path:'/others', element:hasUserAnAccount && !isUserSignOut?<Home/>:<Navigate replace to={'/sign-in'}/>},
-    {path:'/my-acount', element:<MyAcount/>},
-    {path:'/my-order', element:<MyOrder/>},
-    {path:'/my-orders', element:<MyOrders/>},
-    {path:'/my-orders/:id', element:<MyOrder/>},
-    {path:'/sign-In', element:<SignIn/>},
-    {path:'/*', element:<NotFound/>},
+    { path: '/', element: <Home /> },
+    { path: '/clothes', element: hasUserAnAccount && !isUserSignOut ? <Home /> : <Navigate replace to={'/sign-in'} /> },
+    { path: '/electronics', element: hasUserAnAccount && !isUserSignOut ? <Home /> : <Navigate replace to={'/sign-in'} /> },
+    { path: '/furnitures', element: hasUserAnAccount && !isUserSignOut ? <Home /> : <Navigate replace to={'/sign-in'} /> },
+    { path: '/toys', element: hasUserAnAccount && !isUserSignOut ? <Home /> : <Navigate replace to={'/sign-in'} /> },
+    { path: '/others', element: hasUserAnAccount && !isUserSignOut ? <Home /> : <Navigate replace to={'/sign-in'} /> },
+    { path: '/my-acount', element: <MyAcount /> },
+    { path: '/my-order', element: <MyOrder /> },
+    { path: '/my-orders', element: <MyOrders /> },
+    { path: '/my-orders/:id', element: <MyOrder /> },
+    { path: '/sign-In', element: <SignIn /> },
+    { path: '/*', element: <NotFound /> },
   ])
   return routes
 }
@@ -47,9 +48,10 @@ function App() {
   return (
     <ShoppinCartProvider>
       <BrowserRouter>
-        <AppRouters/>
-        <Navbar/>
-        <CheckoutSideMenu/>
+        <AppRouters />
+        <Navbar />
+        <CheckoutSideMenu />
+        <Footer />
       </BrowserRouter>
     </ShoppinCartProvider>
   )
